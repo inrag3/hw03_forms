7e59f5e57dde6e5701726811ca8c0d59bd3d5eeb
inrage <70843287+inrag3@users.noreply.github.com>
2023-02-24 18:58:24 +0300

    Add tests for posts forms

---
+++ yatube/posts/tests/test_forms.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from ..models import Post
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class PostFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост',
+        )
+
+    def setUp(self):
+        self.guest_client = Client()
+        self.authorized_client = Client()
+        self.authorized_client.force_login(PostFormTests.user)
+
+    def test_posts_post_create_successfully(self):
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Тестовый пост',
+        }
+        self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+
+    def test_posts_post_edit_successfully(self):
+        form_data = {
+            'text': 'Изменённый тестовый пост',
+        }
+        posts_count = Post.objects.count()
+        self.authorized_client.post(
+            reverse('posts:post_edit', kwargs={'post_id': 1}),
+            data=form_data,
+            follow=True
+        )
+        self.assertEqual(Post.objects.count(), posts_count)
